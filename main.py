693feaeb3b2a883cad1ec27ad9901e80c2d22ace
Trenton Waters <57243447+Blaze-stack@users.noreply.github.com>
2021-12-09 11:29:56 -0600

    the best password encryption

---
+++ main.py
@@ -0,0 +1,18 @@
+from base64 import b64decode, b64encode
+
+numbs = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+j = input("enter your passwor:   ")
+
+f = open(f'{j}.txt', 'wb')
+
+jfk = b64encode(j.encode("utf-8"))
+
+for i in range(len(numbs)):
+    jfk = b64encode(jfk)
+
+print("writing... ")
+
+f.write(jfk)
+print("done")
+f.close()
