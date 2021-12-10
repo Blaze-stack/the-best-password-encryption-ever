e6faa1065ac4b783459cd767ce7ac7ac4e5e019e
Trenton Waters <57243447+Blaze-stack@users.noreply.github.com>
2021-12-10 09:24:21 -0600

    more user friendly

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from base64 import b64decode, b64encode
 
 numbs = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
@@ -7,6 +8,8 @@ k = input("encrypt or decrypt:   ")
 
 if k == "encrypt":
     j = input("enter your password:   ")
+
+    tst = time.process_time()
 
     f = open(f'{j}.txt', 'wb')
 
@@ -18,22 +21,30 @@ if k == "encrypt":
     print("writing... ")
 
     f.write(jfk)
+    print(f'time taken {time.process_time() - tst} sec')
     print("done")
     f.close()
 elif k == "decrypt":
     j = input("enter file name - the .txt")
 
+    tst = time.process_time()
+
     f = open(f'{j}.txt', 'rb')
 
     c = f.read()
 
-    jfk = b64decode(c.decode("utf-8"))
+    jfk = b64decode(c)
 
     for i in range(len(numbs)):
         jfk = b64decode(jfk)
-
     
-    print(jfk)
+    jfk = jfk.decode("utf-8")
+
+    print("done.")
+    print()
+    print(f'time taken {time.process_time() - tst}')
+    print()
+    print(f'your decypted password is: {jfk}')
 
     f.close()
 
